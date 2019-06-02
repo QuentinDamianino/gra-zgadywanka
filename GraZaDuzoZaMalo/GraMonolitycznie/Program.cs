7bd98c0e39ff7d07d39e3d1c49595cf187d08932
unknown <brdej98@gmail.com>
2019-06-02 16:55:48 +0200

    feat: Core functionality of a program

--- GraZaDuzoZaMalo/GraMonolitycznie/Program.cs
+++ GraZaDuzoZaMalo/GraMonolitycznie/Program.cs
@@ -10,6 +10,46 @@ namespace GraMonolitycznie
     {
         static void Main(string[] args)
         {
+            Random random = new Random();
+            int randomNumber = random.Next(100);
+            int number;
+            int counter = 0;
+
+            Console.WriteLine("Witaj w grze.\nWybierz liczbę z zakesu 1-100:");
+
+            while(true)
+            {
+                counter++;
+                number = Convert.ToInt32(Console.ReadLine());
+                if (number == randomNumber)
+                {
+                    Console.WriteLine("Brawo wygrałeś");
+                    break;
+                }
+                else if(number > randomNumber)
+                {
+                    Console.WriteLine("Za dużo");
+                    continue;
+                }
+                else if(number < randomNumber)
+                {
+                    Console.WriteLine("Za mało");
+                    continue;
+                }
+                else if(number > 100 || number < 1)
+                {
+                    Console.WriteLine("Podana liczba nie mieści się w zakresie");
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("Nieoczekiwany błąd");
+                    continue;
+                }
+            }
+            
+            Console.WriteLine("Udało ci się zgadnąć w " + counter + " próbach");
+            Console.ReadKey();
         }
     }
 }
